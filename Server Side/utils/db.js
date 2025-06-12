7ba80cb1011addbceffb387e123197285c9fa691
prapuldev <prapulb.dev@gmail.com>
2025-06-12 14:04:50 +0530

    env changes

--- Server Side/utils/db.js
+++ Server Side/utils/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Qwerty@123",
-  database: "employeems",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 con.connect(function (err) {
