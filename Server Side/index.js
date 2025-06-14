4c8fd54b7f600f1098f10e20e6fa3189ec54aa05
prapuldev <prapulb.dev@gmail.com>
2025-06-14 10:44:30 +0530

    portal-application-git

--- Server Side/index.js
+++ Server Side/index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 const app = express() 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","http://dev.reactapp.com"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
--- Server Side/utils/db.js
+++ Server Side/utils/db.js
@@ -3,15 +3,15 @@ import mysql from "mysql";
 const con = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "Qwerty@123",
+  password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "employeems",
 });
 
 con.connect(function (err) {
   if (err) {
-    console.log("connection error");
+    console.error("connection error:", err);
   } else {
-    console.log("Connected");
+    console.log("MySQL Connected");
   }
 });
 
